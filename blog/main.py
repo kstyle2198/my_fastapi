656ecbe0d2f6d9f9c61eee2f2b83eaee9639d49f
Kstyle0710 <63958865+Kstyle0710@users.noreply.github.com>
2022-10-02 15:10:46 +0900

    Creat Blog Get API

--- blog/main.py
+++ blog/main.py
@@ -15,10 +15,22 @@ def get_db():
         db.close()
 
 
-@app.post("/blog")   # create Post API
+@app.post("/blog", tags= ["Blog"])   # create Post API
 def create_post(request: schemas.Blog, db: Session= Depends(get_db)):
     new_blog = models.Blog(title=request.title, description=request.description)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+@app.get("/blog", tags= ["Blog"])
+def all(db:Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+@app.get("/blog/{id}", tags= ["Blog"])
+def get_post(id:str, db:Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id==id).first()
+    return blog
+    
+    

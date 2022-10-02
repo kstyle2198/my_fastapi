f952339d47ac8da42636db62b5128e1e2ba30de2
Kstyle0710 <63958865+Kstyle0710@users.noreply.github.com>
2022-10-02 17:22:08 +0900

    create Blog Update API

--- blog/main.py
+++ blog/main.py
@@ -28,7 +28,7 @@ def all(db:Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get("/blog/{id}", status_code=200, tags= ["Blog"])
+@app.get("/blog/{id}", status_code=status.HTTP_200_OK, tags= ["Blog"])
 def get_post(id:str, db:Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id==id).first()
     if not blog:
@@ -37,8 +37,22 @@ def get_post(id:str, db:Session = Depends(get_db)):
 
 @app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT, tags=["Blog"])
 def delete_post(id:str, db:Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id==id).delete(synchronize_session=False)
+    blog = db.query(models.Blog).filter(models.Blog.id==id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
+    else:
+        blog.delete(synchronize_session=False)
     db.commit()
-    return "done"
+    return "deleted successfully"
+
+@app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED, tags=["Blog"])
+def update_post(id:str, request: schemas.Blog, db: Session= Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id==id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
+    else:
+        blog.update(request.dict())
+    db.commit()
+    return 'updated successfully'
     
     

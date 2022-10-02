12b3569405bb782b7a1f38b8cfa53caaf105eac9
Kstyle0710 <63958865+Kstyle0710@users.noreply.github.com>
2022-10-02 15:30:02 +0900

    applying HTTPException for status code

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, HTTPException
 from . import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -15,7 +15,7 @@ def get_db():
         db.close()
 
 
-@app.post("/blog", tags= ["Blog"])   # create Post API
+@app.post("/blog", tags= ["Blog"], status_code=status.HTTP_201_CREATED)   # create Post API
 def create_post(request: schemas.Blog, db: Session= Depends(get_db)):
     new_blog = models.Blog(title=request.title, description=request.description)
     db.add(new_blog)
@@ -28,9 +28,11 @@ def all(db:Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get("/blog/{id}", tags= ["Blog"])
+@app.get("/blog/{id}", status_code=200, tags= ["Blog"])
 def get_post(id:str, db:Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id==id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Blog with id{id} not found")
     return blog
     
     

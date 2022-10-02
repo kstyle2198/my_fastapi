bfb51df97d758c525ea8aabbb8f842b70bfe1de4
Kstyle0710 <63958865+Kstyle0710@users.noreply.github.com>
2022-10-02 15:41:48 +0900

    create Blog Delete API

--- blog/main.py
+++ blog/main.py
@@ -32,7 +32,13 @@ def all(db:Session = Depends(get_db)):
 def get_post(id:str, db:Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id==id).first()
     if not blog:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Blog with id{id} not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
     return blog
+
+@app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT, tags=["Blog"])
+def delete_post(id:str, db:Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id==id).delete(synchronize_session=False)
+    db.commit()
+    return "done"
     
     
